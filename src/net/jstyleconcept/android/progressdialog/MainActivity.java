6e09459ca5394c3db4683c8626cfcfbd33ebadbd
JStyleConcept <jstyleconcept@gmail.com>
2012-12-21 00:17:55 +0800

    2012/12/21

--- src/net/jstyleconcept/android/progressdialog/MainActivity.java
+++ src/net/jstyleconcept/android/progressdialog/MainActivity.java
@@ -10,15 +10,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	private Button startButton;
+	private Button startButton1,startButton2;
 	private ProgressDialog progressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_mainactivity);
-		startButton=(Button)findViewById(R.id.button1);
-		startButton.setOnClickListener(new Button.OnClickListener() {
+		startButton1=(Button)findViewById(R.id.button1);
+		startButton2=(Button)findViewById(R.id.button2);
+		startButton1.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -30,6 +31,31 @@ public class MainActivity extends Activity {
 						}
 						catch(Exception e) {
 
+						}
+						progressDialog.dismiss();
+					}
+				}.start();
+			}
+		});
+		startButton2.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				progressDialog=new ProgressDialog(MainActivity.this);
+				progressDialog.setTitle("Progress Dialog");
+				progressDialog.setMessage("Please wait ...");
+				progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+				progressDialog.show();
+				new Thread() {
+					public void run() {
+						for(int i=0;i<100;i+=10) {
+							try {
+								sleep(500);
+							}
+							catch(Exception e) {
+
+							}
+							progressDialog.setProgress(i);
 						}
 						progressDialog.dismiss();
 					}
